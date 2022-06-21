536a3ccfdc42b4f5c77982c18fed532adbede955
Ilia <ilytar@gmail.com>
2022-06-21 21:24:13 +0300

    Используем фрагмент для добавления фото

--- js/miniatures.js
+++ js/miniatures.js
@@ -3,13 +3,14 @@ import {createPhotos} from './data.js';
 const containerPhotos = document.querySelector('.pictures');
 const similarPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const similarPhotos = createPhotos();
+const similarListFragment = document.createDocumentFragment();
 
 similarPhotos.forEach((photo) => {
   const photoElement = similarPhotoTemplate.cloneNode(true);
   photoElement.querySelector('.picture__likes').textContent = photo.likes;
   photoElement.querySelector('.picture__img').src = photo.url;
   photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-  containerPhotos.appendChild(photoElement);
+  similarListFragment.appendChild(photoElement);
 });
+containerPhotos.appendChild(similarListFragment);
 
-// console.log(createPhotos());
