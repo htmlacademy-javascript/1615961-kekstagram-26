1f627aec6c2c58cc13db4a0e35593ea3acbf3cd7
Ilia <ilytar@gmail.com>
2022-07-06 17:22:09 +0300

    Добавляет возможность масштабировать изображение

--- js/scale.js
+++ js/scale.js
@@ -1,6 +1,7 @@
 const scaleButtonMinus = document.querySelector('.scale__control--smaller');
 const scaleButtonPlus = document.querySelector('.scale__control--bigger');
 const scaleField = document.querySelector('.scale__control--value');
+const pictureElement = document.querySelector('.img-upload__preview-picture');
 
 let scaleFieldValue = 100;
 scaleField.value = `${scaleFieldValue }%`;
@@ -9,6 +10,35 @@ function scalePlus () {
   if (scaleFieldValue <= 75) {
     scaleFieldValue = Number(scaleFieldValue) + 25;
     scaleField.value = `${scaleFieldValue }%`;
+
+    if (scaleFieldValue === 25) {
+      pictureElement.classList.remove('img-upload__preview-picture--scale50');
+      pictureElement.classList.remove('img-upload__preview-picture--scale75');
+      pictureElement.classList.remove('img-upload__preview-picture--scale100');
+      pictureElement.classList.add('img-upload__preview-picture--scale25');
+    }
+
+    if (scaleFieldValue === 50) {
+      pictureElement.classList.remove('img-upload__preview-picture--scale25');
+      pictureElement.classList.remove('img-upload__preview-picture--scale75');
+      pictureElement.classList.remove('img-upload__preview-picture--scale100');
+      pictureElement.classList.add('img-upload__preview-picture--scale50');
+    }
+
+    if (scaleFieldValue === 75) {
+      pictureElement.classList.remove('img-upload__preview-picture--scale25');
+      pictureElement.classList.remove('img-upload__preview-picture--scale50');
+      pictureElement.classList.remove('img-upload__preview-picture--scale100');
+      pictureElement.classList.add('img-upload__preview-picture--scale75');
+    }
+
+    if (scaleFieldValue === 100) {
+      pictureElement.classList.remove('img-upload__preview-picture--scale25');
+      pictureElement.classList.remove('img-upload__preview-picture--scale50');
+      pictureElement.classList.remove('img-upload__preview-picture--scale75');
+      pictureElement.classList.add('img-upload__preview-picture--scale100');
+    }
+
     return `${scaleFieldValue  }%`;
   }
 }
@@ -17,6 +47,35 @@ function scaleMinus () {
   if (scaleFieldValue >= 50) {
     scaleFieldValue = scaleFieldValue - 25;
     scaleField.value = `${scaleFieldValue }%`;
+
+    if (scaleFieldValue === 25) {
+      pictureElement.classList.remove('img-upload__preview-picture--scale50');
+      pictureElement.classList.remove('img-upload__preview-picture--scale75');
+      pictureElement.classList.remove('img-upload__preview-picture--scale100');
+      pictureElement.classList.add('img-upload__preview-picture--scale25');
+    }
+
+    if (scaleFieldValue === 50) {
+      pictureElement.classList.remove('img-upload__preview-picture--scale25');
+      pictureElement.classList.remove('img-upload__preview-picture--scale75');
+      pictureElement.classList.remove('img-upload__preview-picture--scale100');
+      pictureElement.classList.add('img-upload__preview-picture--scale50');
+    }
+
+    if (scaleFieldValue === 75) {
+      pictureElement.classList.remove('img-upload__preview-picture--scale25');
+      pictureElement.classList.remove('img-upload__preview-picture--scale50');
+      pictureElement.classList.remove('img-upload__preview-picture--scale100');
+      pictureElement.classList.add('img-upload__preview-picture--scale75');
+    }
+
+    if (scaleFieldValue === 100) {
+      pictureElement.classList.remove('img-upload__preview-picture--scale25');
+      pictureElement.classList.remove('img-upload__preview-picture--scale50');
+      pictureElement.classList.remove('img-upload__preview-picture--scale75');
+      pictureElement.classList.add('img-upload__preview-picture--scale100');
+    }
+
     return `${scaleFieldValue  }%`;
   }
 }
