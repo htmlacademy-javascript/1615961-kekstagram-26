29fcaa9840b04632414a7aa3f52728c67b6f76da
Ilia <ilytar@gmail.com>
2022-07-10 20:24:59 +0300

    Добавляет фетч, отрисовывает миниатюры и большие фотографии из данных с сервера

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 import {createPhotos} from './data.js';
 import {getRandomInteger, isEscapeKey} from './utilites.js';
-import './miniatures.js';
+import {renderPhotos} from './miniatures.js';
 import './big-photo.js';
 import './upload-image.js';
 import './user-form.js';
 import './scale.js';
 import './photo-effects.js';
+
+
+fetch('https://26.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((photos) => {
+    console.log(photos);
+    renderPhotos(photos);
+  });
