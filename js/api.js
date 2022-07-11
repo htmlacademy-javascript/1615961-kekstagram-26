f442395d4bec607fbfa0d81ff93bcd2feb6aa53a
Ilia <ilytar@gmail.com>
2022-07-11 19:45:02 +0300

    Выносит адреса серверов в переменные

--- js/api.js
+++ js/api.js
@@ -1,15 +1,17 @@
+const addressGetServer = 'https://26.javascript.pages.academy/kekstagram/data';
+const addressPostServer = 'https://26.javascript.pages.academy/kekstagram';
+
 const getData = (onSuccess) => {
-  fetch('https://26.javascript.pages.academy/kekstagram/data')
+  fetch(addressGetServer)
     .then((response) => response.json())
     .then((photos) => {
       onSuccess(photos);
-    }
-    );
+    });
 };
 
 const sendData = (onSuccess, onFail, body) => {
 
-  fetch('https://26.javascript.pages.academy/kekstagram',
+  fetch(addressPostServer,
     {
       method: 'POST',
       body,
