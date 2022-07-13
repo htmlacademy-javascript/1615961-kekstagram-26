9c1d2f813a1cac8083583f496705875393f2dd04
Ilia <ilytar@gmail.com>
2022-07-13 14:27:26 +0300

    Добавляет возможность комментирования фотографий

--- js/big-photo.js
+++ js/big-photo.js
@@ -22,4 +22,23 @@ bigPictureClose.addEventListener('click', ()=> {
   closeBigPicture();
 });
 
+// Добавление комментария
+
+const socialCommentsList = document.querySelector('.social__comments');
+const socialFooterCommentField = document.querySelector('.social__footer-text');
+const socialFooterSendButton = document.querySelector('.social__footer-btn');
+
+socialFooterSendButton.addEventListener('click', () => {
+  const commentElementTemplate = document.querySelector('#social__comment').content.querySelector('.social__comment');
+  const commentElement = commentElementTemplate.cloneNode(true);
+  const textMyComment = commentElement.querySelector('.social__text');
+  const socialFooterAuthorAvatar = commentElement.querySelector('.social__picture');
+  if (socialFooterCommentField.value !== '') {
+    textMyComment.textContent = socialFooterCommentField.value;
+    socialFooterCommentField.value = '';
+    socialFooterAuthorAvatar.src = 'img/avatar-6.svg';
+    socialCommentsList.appendChild(commentElement);
+  }
+});
+
 export {onBigPictureEscKeydown};
