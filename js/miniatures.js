716f535883f83f7c40b0e437930aea3ac6f62587
Ilia <ilytar@gmail.com>
2022-07-13 17:22:29 +0300

    Добавляет отображение комментариев по 5 штук с возможностью дозагрузки по 5 штук

--- js/miniatures.js
+++ js/miniatures.js
@@ -9,9 +9,11 @@ const bigPictureLikes = bigPicture.querySelector('.likes-count');
 const bigPictureDescription = bigPicture.querySelector('.social__caption');
 const bigPictureCommentsList = bigPicture.querySelector('.social__comments');
 const bigPictureCommentsCount = bigPicture.querySelector('.comments-count');
-// const bigPictureCommentsCounter = bigPicture.querySelector('.social__comment-count');
+const bigPictureCommentsCounter = bigPicture.querySelector('.social__comment-count');
+const bigPictureCommentsLoader = bigPicture.querySelector('.social__comments-loader');
 
 const commentElementTemplate = document.querySelector('#social__comment').content.querySelector('.social__comment');
+const MAX_SHOWN_COMMEN_AMOUNT = 5;
 
 const renderPhotos = function (photos) {
   photos.forEach(({url, likes, comments, description}) => {
@@ -30,12 +32,47 @@ const renderPhotos = function (photos) {
       bigPictureCommentsCount.textContent = comments.length;
       bigPictureCommentsList.innerHTML = '';
 
-      comments.forEach(({avatar, message, name}) => {
-        const commentElement = commentElementTemplate.cloneNode(true);
-        bigPictureCommentsList.appendChild(commentElement);
-        commentElement.querySelector('.social__text').textContent = message;
-        commentElement.querySelector('.social__picture').src = avatar;
-        commentElement.querySelector('.social__picture').alt = name;
+      if (comments.length <= MAX_SHOWN_COMMEN_AMOUNT) {
+        comments.forEach(({avatar, message, name}) => {
+          bigPictureCommentsLoader.classList.add('hidden');
+          const commentElement = commentElementTemplate.cloneNode(true);
+          bigPictureCommentsList.appendChild(commentElement);
+          commentElement.querySelector('.social__text').textContent = message;
+          commentElement.querySelector('.social__picture').src = avatar;
+          commentElement.querySelector('.social__picture').alt = name;
+          bigPictureCommentsCounter.textContent = `${comments.length} из ${comments.length} комментариев`;
+        });
+      }
+
+      if (comments.length > MAX_SHOWN_COMMEN_AMOUNT) {
+        comments.slice(0, MAX_SHOWN_COMMEN_AMOUNT).forEach(({avatar, message, name}) => {
+          bigPictureCommentsLoader.classList.remove('hidden');
+          const commentElement = commentElementTemplate.cloneNode(true);
+          bigPictureCommentsList.appendChild(commentElement);
+          commentElement.querySelector('.social__text').textContent = message;
+          commentElement.querySelector('.social__picture').src = avatar;
+          commentElement.querySelector('.social__picture').alt = name;
+          bigPictureCommentsCounter.textContent = `${MAX_SHOWN_COMMEN_AMOUNT} из ${comments.length} комментариев`;
+        });
+      }
+
+      let shownComments = MAX_SHOWN_COMMEN_AMOUNT;
+      bigPictureCommentsLoader.addEventListener('click', () => {
+        shownComments = shownComments + 5;
+        comments.innerHTML = '';
+        bigPictureCommentsCounter.textContent = `${shownComments} из ${comments.length} комментариев`;
+        comments.slice(shownComments - MAX_SHOWN_COMMEN_AMOUNT, shownComments).forEach(({avatar, message, name}) => {
+          bigPictureCommentsLoader.classList.remove('hidden');
+          const commentElement = commentElementTemplate.cloneNode(true);
+          bigPictureCommentsList.appendChild(commentElement);
+          commentElement.querySelector('.social__text').textContent = message;
+          commentElement.querySelector('.social__picture').src = avatar;
+          commentElement.querySelector('.social__picture').alt = name;
+        });
+        if (shownComments >= comments.length) {
+          bigPictureCommentsLoader.classList.add('hidden');
+          bigPictureCommentsCounter.textContent = `${comments.length} из ${comments.length} комментариев`;
+        }
       });
     });
   });
