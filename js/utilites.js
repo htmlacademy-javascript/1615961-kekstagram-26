713c3501c2fa87aec2baa58dc389372830f4bff1
Ilia <ilytar@gmail.com>
2022-07-17 17:40:58 +0300

    Б12

--- js/utilites.js
+++ js/utilites.js
@@ -23,19 +23,6 @@ const showAlert = function (message) {
   }, ALERT_SHOW_TIME);
 };
 
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
-
-function getRandomInteger(min, max) {
-  if (min > max) {
-    [min, max] = [max, min];
-  }
-  if (min < 0 || max < 0) {
-    throw new RangeError('Параметры должны быть больше нуля');
-  }
-  const random = min + Math.random() * (max + 1 - min);
-  return Math.floor(random);
-}
-
 // Функция для проверки нажатия Esc.
 
 function isEscapeKey (evt) {
